02b1ddeaf1829f135dffc247839a6224dd5bff68
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-02-19 06:26:19 -0600

    partially functioning password generator feature, need to fix the array of arrays randomization and character length to match user input

--- script.js
+++ script.js
@@ -10,21 +10,22 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Creating a shuffle for randomizing which character array is selected
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
 
 // Generates password
 function generatePassword() {
   var i = ""
-
   // User decision inputs
   var charCount = prompt("How many characters do you want in your password? Please enter a number between 8 and 128 characters.");
   var upperCase = confirm("Do you want upper case letters included?");
   var numbers = confirm("Do you want numbers included?")
   var specialCharacters = confirm("Do you want special characters included?");
 
-  // Establishing an array of arrays
-  var collectiveValues = (upperCaseValue, numbersValue, specialCharactersValue);
-
-// User input determining length of password, converted to number format
+  
+// User input determining length of password, converted to number format if in range
   if (charCount >=8 && charCount <=128) {
     var charCountValue = parseInt(charCount);
   }
@@ -53,31 +54,61 @@ function generatePassword() {
   specialCharactersValue = void(0);
   }
 
-  var randomizeUpperCaseValue = upperCaseValue[Math.floor(Math.random() * upperCaseValue.length)];
-  var randomizeNumbers = numbersValue[Math.floor(Math.random() * numbersValue.length)];
-  var randomizeSpecialCharacters = specialCharactersValue[Math.floor(Math.random() * specialCharactersValue.length)];
-  
-// for (i = 0; i < cars.length; i++) {
-//   text += cars[i] + "<br>";
-// }
-  console.log(randomizeUpperCaseValue, randomizeNumbers, randomizeSpecialCharacters);
-  console.log(upperCaseValue, numbersValue, specialCharactersValue, charCountValue);
-  console.log(collectiveValues);
-}
-// // // Taken from StackOverflow (https://stackoverflow.com/questions/19269545/how-to-get-n-no-elements-randomly-from-an-array)
-// // function getRandom(arr, n) {
-// //   var result = new Array(n),
-// //       len = arr.length,
-// //       taken = new Array(len);
-// //   if (n > len)
-// //       throw new RangeError("getRandom: more elements taken than available");
-// //   while (n--) {
-// //       var x = Math.floor(Math.random() * len);
-// //       result[n] = arr[x in taken ? taken[x] : x];
-// //       taken[x] = --len in taken ? taken[len] : len;
-// //   }
-// //   return result;
-// }
+  // Establishing an array of arrays
+  var collectiveValues = [
+    (upperCaseValue),
+    (numbersValue),
+    (specialCharactersValue),
+  ]
 
+  // Counting the digits in charCountValue to have a string length for generator function to know when to end
+  var charCountValue_toLength = charCountValue.toPrecision(charCountValue);
+
+  if (charCountValue_toLength >=100) {
+    charCountValue_toLength = charCountValue_toLength.substring(0, (charCountValue_toLength -3));
+  }
+  else if (charCountValue_toLength >= 10) {
+    charCountValue_toLength = charCountValue_toLength.slice(0, (charCountValue_toLength -2));
+  }
+  else if (charCountValue_toLength < 10) {
+    charCountValue_toLength = charCountValue_toLength.slice(2, charCountValue);
+  }
+// Putting password together by randomizing which array is picked, and then randomizing the selection from each array until length is reached
+  for (i = 0; i < charCountValue_toLength.length; i++) {
+    var completedPassword = "";
+    checkPasswordLength:
+    while (completedPassword.length <= charCountValue_toLength.length) {
+
+      shuffle(collectiveValues);
+
+        var randomizeUpperCaseValue = upperCaseValue[Math.floor(Math.random() * upperCaseValue.length)];
+  
+          var completedPassword = completedPassword.concat(randomizeUpperCaseValue);
+  
+        var randomizeNumbers = numbersValue[Math.floor(Math.random() * numbersValue.length)];
+  
+          var completedPassword = completedPassword.concat(randomizeNumbers);
+  
+        var randomizeSpecialCharacters = specialCharactersValue[Math.floor(Math.random() * specialCharactersValue.length)];
+  
+          var completedPassword = completedPassword.concat(randomizeSpecialCharacters);
+
+        if (completedPassword.length !== charCountValue_toLength.length) {
+          continue checkPasswordLength;
+        }
+    }
+    // used to log the character length in relation to what it should be based off of what the user entered
+    function getCharacterLength (str) {
+      return [...str].length;
+    }
+    console.log(getCharacterLength(completedPassword));
+    console.log(randomizeUpperCaseValue, randomizeNumbers, randomizeSpecialCharacters);
+    console.log(upperCaseValue, numbersValue, specialCharactersValue, charCountValue);
+    console.log(collectiveValues);
+    console.log(charCountValue_toLength);
+    console.log(completedPassword);
+    return completedPassword;
+  }
+} 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
